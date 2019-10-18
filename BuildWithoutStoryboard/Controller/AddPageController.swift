26e90e6a50d34bb7787883e99a9f80731172c977
Ajisaputrars <Ajisaputrars@hotmail.com>
2019-10-18 13:51:38 +0700

    fix(textfield showing keyboard 2): fixing bug in keyboard

--- BuildWithoutStoryboard/Controller/AddPageController.swift
+++ BuildWithoutStoryboard/Controller/AddPageController.swift
@@ -13,26 +13,44 @@ class AddPageController: UIViewController, UITextFieldDelegate {
     var addPageView: AddPageView!
     
     weak var delegate: SaveStudentDelegate?
+    
+    var keyboardHeight: CGFloat!
+    
+    var activeTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        addPageView = AddPageView(frame: view.frame)
+        print("View Did Load")
+
+        addPageView = AddPageView()
         self.view = addPageView
         self.view.backgroundColor = .white
         
         addPageView.nameTextField.delegate = self
         addPageView.infoTextField.delegate = self
         addPageView.descriptionTextField.delegate = self
+        addPageView.sampleTextField.delegate = self
         
         addPageView.saveButton.addTarget(self, action: #selector(addTapped), for: .touchUpInside)
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(cancelTapped))
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     @objc private func addTapped(){
         let student = Student(name: addPageView.nameTextField.text, description: addPageView.descriptionTextField.text, info: addPageView.infoTextField.text)
         self.delegate?.savedStudent(student: student)
-        
+
         self.dismiss(animated: true, completion: nil)
     }
     
@@ -40,12 +58,33 @@ class AddPageController: UIViewController, UITextFieldDelegate {
         self.dismiss(animated: true, completion: nil)
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        resignFirstResponder()
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        self.activeTextField = textField
+        return true
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
+        self.activeTextField = nil
         return true
+    }
+    
+    @objc private func keyboardWillChange(notification: Notification){
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            
+            let shouldUp = (activeTextField.frame.origin.y - keyboardRect.height ) - activeTextField.frame.height - 30
+            
+            if shouldUp >= 0 {
+                view.bounds.origin.y = shouldUp
+            }
+            self.keyboardHeight = -keyboardRect.height
+            
+        } else {
+            view.bounds.origin.y = 0
+        }
     }
 }
